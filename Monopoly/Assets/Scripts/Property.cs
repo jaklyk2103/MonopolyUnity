26aacf836a7f6ca3742836830ea836df65741cb6
Tomasz Kus <tomasz.kus97@gmail.com>
2019-03-21 14:12:28 +0100

    work in progress

---
+++ Monopoly/Assets/Scripts/Property.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+
+public class Property : MonoBehaviour
+{
+    public int CountryId { get; set; }
+    public int? OwnerId { get; set; }
+    public int NumberOfHouses
+    { get; set; }
+    public int Price;
+    public int HousePrice;
+    public int HotelPrice;
+    public int Rent;
+    public int RentPerHouse;
+    bool HotelBuilt;
+    public int HotelRent;
+    
+
+    void BuyProperty(int ownerId)
+    {
+        if (OwnerId==null)
+        {
+            OwnerId = ownerId;
+        }
+    }
+    void BuildHouse()
+    {
+        if(NumberOfHouses < 4)
+        {
+            NumberOfHouses++;
+        }
+    }
+    void BuildHotel()
+    {
+        if (NumberOfHouses == 4)
+        {
+            HotelBuilt = true;
+        }
+    }
+    int GetRent()
+    {
+        if (HotelBuilt)
+        {
+            return Rent + HotelRent;
+        }
+        else
+        {
+            return Rent + NumberOfHouses * RentPerHouse;
+        }
+    }
+}
